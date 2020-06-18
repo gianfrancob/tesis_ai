11c7196f64437c4582572878107506a6cb0ce29d
matrix_gianfranco <gianfranco.barbiani@gmail.com>
2020-06-18 18:16:48 -0300

    Added nvidia runtime to run_container.sh to be able to use tensorflow

--- docker-gpu/helper_scripts/run_container.sh
+++ docker-gpu/helper_scripts/run_container.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-CONTAINER="docker run -it --ipc=host --net=host -v ~/workspace/ml_gpu_docker_files/:/ds hamelsmu/ml-gpu"
+CONTAINER="docker run -it --rm --runtime=nvidia --ipc=host --net=host -v ~/workspace/ml_gpu_docker_files/:/ds hamelsmu/ml-gpu"
 echo 'Starting container with commmand: '$CONTAINER
 eval $CONTAINER
