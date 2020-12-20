c54fb437432f2939961f0a1f08e662a34484bc68
matrix_gianfranco <gianfranco.barbiani@gmail.com>
2020-12-20 13:50:00 -0300

    WIP

--- yolov3-tf2_gian/tools/map_visualizer.py
+++ yolov3-tf2_gian/tools/map_visualizer.py
@@ -5,9 +5,10 @@ from absl import app, flags, logging
 from absl.flags import FLAGS
 import numpy as np
 from mean_average_precision import MeanAveragePrecision
+import functools 
 
 flags.DEFINE_string('classes', './data/test/ps.names', 'path to classes file')
-flags.DEFINE_string('path', None,
+flags.DEFINE_string('path', './tools/logs.json',
                     'path to json file with ground truth and detections')
 flags.DEFINE_integer('size', 416, 'images size')
 flags.DEFINE_string('output', './output', 'path to output image')
@@ -26,37 +27,68 @@ def main(_argv):
         'path': FLAGS.path,
         'img_size': FLAGS.size,
         'num_classes': FLAGS.num_classes,
-        'output': []
+        'output': ""
     }
+    with open(FLAGS.classes, "r") as classesFile:
+        classes = classesFile.readlines()
+        classesFile.close()
+
 
     with open(FLAGS.path, "r") as logFile:
+        data = json.load(logFile)
+        for out in data["output"]:
+            groundTruth = out["true_detections"]
+            rawPreds = out["detections"]
+            preds = []
+            for p in rawPreds:
+                if (type(p) == type({})):
+                    predClass = None
+                    for i in range(len(classes)):
+                        logs["output"] += str(p["class_name"])
+                        logs["output"] += str(classes[i])
+                        if (id(p["class_name"]) is id(classes[i])):
+                            logs["output"] += str("IGUALES!!!!!!!!!!!!!!!")
+                            predClass = i
+                            break
+                    if (predClass != None):
+                        logs["output"] += "\n" + str("RAW PRED BOXES: " + json.dumps(p["boxes"]))
+                        logs["output"] += "\n" + str("RAW PRED predClass: " + predClass)
+                        preds += p["boxes"].append(predClass)
+                    
+
+            for label in groundTruth + preds:
+                # logs["output"] += "\n" + str("GROUD TRUTH BOXES: " + json.dumps(label))
+                if (functools.reduce(lambda a,b : a+b, label[:-1]) == 0):
+                    # logs["output"] += "\n" + str("deleting...")
+                    del label
+                else:
+                    for i in range(len(label[:-1])):
+                        label[i] = label[i] * data["img_resize"]
+                    logs["output"] += "\n" + str("GROUD TRUTH BOXES AFTER: " + json.dumps(label))
+                    
+        logFile.close()
+            
+
         # [xmin, ymin, xmax, ymax, class_id, difficult, crowd]
-        gt = np.array([
-            [439, 157, 556, 241, 0, 0, 0],
-            [437, 246, 518, 351, 0, 0, 0],
-            [515, 306, 595, 375, 0, 0, 0],
-            [407, 386, 531, 476, 0, 0, 0],
-            [544, 419, 621, 476, 0, 0, 0],
-            [609, 297, 636, 392, 0, 0, 0]
-        ])
+        gt = np.array(groundTruth)
 
         # [xmin, ymin, xmax, ymax, class_id, confidence]
-        preds = np.array([
-            [429, 219, 528, 247, 0, 0.460851],
-            [433, 260, 506, 336, 0, 0.269833],
-            [518, 314, 603, 369, 0, 0.462608],
-            [592, 310, 634, 388, 0, 0.298196],
-            [403, 384, 517, 461, 0, 0.382881],
-            [405, 429, 519, 470, 0, 0.369369],
-            [433, 272, 499, 341, 0, 0.272826],
-            [413, 390, 515, 459, 0, 0.619459]
-        ])
+        preds = np.array(preds)
+
+        logs["output"] += "\n" + str("GROUD TRUTH: " + np.array_str(gt))
+        logs["output"] += "\n" + str("PREDS: " + np.array_str(preds))
+
+        np.savetxt('./tools/mapLogs_gt.csv', gt, delimiter=',')
+        np.savetxt('./tools/mapLogs_preds.csv', preds, delimiter=',')
+
+        with open("./tools/mapLogs.json", "w") as f:
+            f.write(logs["output"])
 
         # create metric_fn
-        metric_fn = MeanAveragePrecision(num_classes=1)
+        metric_fn = MeanAveragePrecision(num_classes=FLAGS.num_classes)
 
         # add some samples to evaluation
-        for i in range(10):
+        for i in range(len(logs["output"])):
             metric_fn.add(preds, gt)
 
         # compute PASCAL VOC metric
@@ -69,8 +101,11 @@ def main(_argv):
         print(f"COCO mAP: {metric_fn.value(iou_thresholds=np.arange(0.5, 1.0, 0.05), recall_thresholds=np.arange(0., 1.01, 0.01), mpolicy='soft')['mAP']}")
 
 
+
 if __name__ == '__main__':
     try:
         app.run(main)
     except SystemExit:
         pass
+
+
